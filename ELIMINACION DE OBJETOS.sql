63f09cc29404b651c0b406fdb4b59b50aab69afb
Ian Carbajal <70449684+iancarbajal@users.noreply.github.com>
2021-11-25 22:07:26 -0600

    Update ELIMINACION DE OBJETOS.sql

--- ELIMINACION DE OBJETOS.sql
+++ ELIMINACION DE OBJETOS.sql
@@ -3,6 +3,6 @@ drop table persona cascade;
 drop table pregunta cascade;
 drop table respuesta cascade;
 drop table tema cascade;
-drop table persona_encuesta,persona_pregunta,persona_pregunta_respuesta,persona_respuesta,pregunta_respuesta;
+drop table persona_encuesta,persona_pregunta_respuesta;
 drop sequence id_persona_persona_seq,encuesta_seq,tema_seq,id_pregunta_pregunta_seq,id_respuesta_respuesta_seq;
 
