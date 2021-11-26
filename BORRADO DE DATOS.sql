590039b7ea474c4bdc31ee07a8d099e68a40b027
Ian Carbajal <70449684+iancarbajal@users.noreply.github.com>
2021-11-25 22:24:39 -0600

    Add files via upload

---
+++ BORRADO DE DATOS.sql
@@ -0,0 +1,5 @@
+delete from respuesta;
+delete from persona;
+delete from pregunta;
+delete from tema;
+delete from encuesta;
---
+++ ELIMINACION DE OBJETOS.sql
@@ -0,0 +1,8 @@
+drop table encuesta cascade;
+drop table persona cascade;
+drop table pregunta cascade;
+drop table respuesta cascade;
+drop table tema cascade;
+drop table persona_encuesta,persona_pregunta_respuesta;
+drop sequence id_persona_persona_seq,encuesta_seq,tema_seq,id_pregunta_pregunta_seq,id_respuesta_respuesta_seq;
+
